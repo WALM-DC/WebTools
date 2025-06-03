955595defeb3c52887bc66ec7463f3ae69aea615
Wallner Matthias <WALM@xxxlutz.at>
2025-06-03 06:20:27 +0200

    Updates

--- AktionspreisFixer/runListExporter.bat
+++ AktionspreisFixer/runListExporter.bat
@@ -1,16 +1,19 @@
 @echo off
-echo Pull ICOM git.
+echo Pull ICOM git?
+PAUSE
 c:
 cd "C:\xxxlutz\IG-Creator\XXXLutz\ICOM"
 git pull
 
 echo Create new List.json.
+PAUSE
 f:
 cd "F:\WebTools\AktionspreisFixer"
 python JsonList.py
 
 echo Push new List.Json to git.
 PAUSE
+f:
 cd "F:\WebTools\public"
 git push
 PAUSE
