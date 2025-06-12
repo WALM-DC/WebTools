0beea52e87699a69e1081237b51d57b52e8a4f79
Wallner Matthias <WALM@xxxlutz.at>
2025-06-12 07:58:05 +0200

    updates

--- AktionspreisFixer/JsonUpdate.py
+++ AktionspreisFixer/JsonUpdate.py
@@ -10,6 +10,8 @@ checkDate = datetime.datetime(2025, 6, 3).strftime('%d.%m.%Y')
 
 # List of column headers we need to extract
 headerList = ["Aktion", "vonDatum", "bisDatum", "NachlassinProzent", "Warengruppe_Kombi", "AusnahmeWarengruppe", "StatKZ_Kombi", "Marken", "ausmark", "AusnahmeLieferant"]
+MKSAKTobject = {}
+prodGroups = []
 
 # if dateToday >= checkDate:
 #     print('Today is >=')
@@ -18,66 +20,33 @@ headerList = ["Aktion", "vonDatum", "bisDatum", "NachlassinProzent", "Warengrupp
 #     print('CheckDate is <')
 #     print(checkDate)
 
-# Reads MKSAKT file and convert to object
-def read_csv_file():
-    headerIndex = []
-    MKSAKTobject = {}
-
-    with open(r'F:\WebTools\AktionspreisFixer\MKSAKT_1_test.csv', 'r') as csvfile:
-        reader = csv.reader(csvfile)
-        i = 0
-        for row in reader:
-            for index, column in enumerate(f"{row}".split(';')):
-                if index in headerIndex:
-                    if "1/4/" in f"{row}":
-                        MKSAKTobject.get(f"{headerList[headerIndex.index(index)]}").append(column)
-                        i+=1
-                if column in headerList:
-                    if index not in headerIndex:
-                        headerIndex.append(index)
-                        tempObject = {
-                            column:[]
-                        }
-                    MKSAKTobject.update(tempObject)
-
-        print (MKSAKTobject)
-        print(f"{i}"+" entries imported.")
-
-# Build logic to find models to update - loop through object line by line
-#   - Check valid date ragen
-#   - Check valid product groups
-#       - Check against excluded product groups, brand & deliverers
-#   - Produce preview output list
-
 def save_json_file(data):
     # Output file path for log
     # requires datestamp
-    output_path = (r"F:\WebTools\AktionspreisFixer\log.text")
+    output_path = (r"F:\WebTools\AktionspreisFixer\testFilterList.json")
 
     # Write JSON object to file
     with open(output_path, 'w', encoding='utf-8') as f:
         json.dump(data, f, indent=2, ensure_ascii=False)
 
-    data['settings']['aktion'] = 20
+    # data['settings']['aktion'] = 20
 
 # Collects 3D model data
-def find_json_with_values(root_path, filter_path):
+def find_json_with_values(root_path, prodGroups):
     """
     Walk through all folders starting from root_path, parse JSON files,
     """
-
-    filterList = {}
+    # filterList = {}
     fullModelList = {}
     fullConditionsList = {}
 
-    try:
-        with open(filter_path, 'r', encoding='utf-8') as f:
-            data = json.load(f)
-            filterList = data
-            print(filterList.get('Rabatt', {}).get('Prozent'))
-    except Exception as e:
-        print(f"[ERROR] Unexpected error with {filter_path}: {e}")
-
+    # try:
+    #     with open(filter_path, 'r', encoding='utf-8') as f:
+    #         data = json.load(f)
+    #         filterList = data
+    #         print(filterList.get('Rabatt', {}).get('Prozent'))
+    # except Exception as e:
+    #     print(f"[ERROR] Unexpected error with {filter_path}: {e}")
     for dirpath, _, filenames in os.walk(root_path):
         for filename in filenames:
             if filename.endswith('.json') and filename.find(' ')==-1:
@@ -87,26 +56,26 @@ def find_json_with_values(root_path, filter_path):
                         data = json.load(f)
                         
                         # if model.entlastung < submitted aktion & if model.criteria fit:
-
                         if contains_target_keys(data, {'catalog'}):
-                            model_object = {
-                                data['catalog'].split('.')[0] +'.'+ filename.split('.')[0]: {
-                                    'filePath': file_path,
-                                    'fileName': filename.split('.')[0],
-                                    'productNumber': '',
-                                    'locale': filename.split('@')[1].split('.')[0],
-                                    'brand': data.get('brand', ''),
-                                    'brandName': data.get('brandName', ''),
-                                    'productGroup': data.get('productGroup', ''),
-                                    'settings': {
-                                        'preis': data.get('settings',{}).get('kaa1', ''),
-                                        'entlasgung': data.get('settings',{}).get('entlastung', ''),
-                                        'aktion': data.get('settings',{}).get('aktion', ''),
-                                        'pricing': data.get('settings', {}).get('pricing', '')
+                            if data["productGroup"] in prodGroups:
+                                model_object = {
+                                    data['catalog'].split('.')[0] +'.'+ filename.split('.')[0]: {
+                                        'filePath': f'{file_path}',
+                                        'fileName': filename.split('.')[0],
+                                        'productNumber': '',
+                                        'locale': filename.split('@')[1].split('.')[0],
+                                        'brand': data.get('brand', ''),
+                                        'brandName': data.get('brandName', ''),
+                                        'productGroup': data.get('productGroup', ''),
+                                        'settings': {
+                                            'preis': data.get('settings',{}).get('kaa1', ''),
+                                            'entlasgung': data.get('settings',{}).get('entlastung', ''),
+                                            'aktion': data.get('settings',{}).get('aktion', ''),
+                                            'pricing': data.get('settings', {}).get('pricing', '')
+                                        }
                                     }
                                 }
-                            }
-                            fullModelList.update(model_object)
+                                fullModelList.update(model_object)
 
                         if filename.startswith('conditions'):
                             conditions_object = data['sets']
@@ -126,12 +95,61 @@ def find_json_with_values(root_path, filter_path):
         except KeyError as e:
             print(f'KeyError: {e} not found in conditions!')
     
-    print(fullModelList)
-
-    # save_json_file(fullModelList)
-
+    save_json_file(fullModelList)
 # Loop through 3D models, check against MKSAKT object and date
 # Overwrite fitting 3D models
+
+def filter_models_byProdGroup(root_path, prodGroups):
+    for group in MKSAKTobject["Warengruppe_Kombi"]:
+        if not prodGroups:
+            prodGroups = group.split('/')
+        else:
+            split_list = group.split('/')
+            prod_list = prodGroups
+            prodGroups += sorted(x for x in split_list if x not in set(prod_list))
+
+    find_json_with_values(root_path, prodGroups)
+
+# Reads MKSAKT file and convert to object
+def read_csv_file(root_path, prodGroups):
+    headerIndex = []
+    csvRead = {}
+    with open(r'F:\WebTools\AktionspreisFixer\MKSAKT_1_test.csv', 'r') as csvfile:
+        reader = csv.reader(csvfile)
+        i = 1
+        for row in reader:
+            for index, column in enumerate(f"{row}".split(';')):
+                if index in headerIndex:
+                    if "1/4/" in f"{row}":
+                        csvRead.get(f"{headerList[headerIndex.index(index)]}").append(column)
+                        i+=1
+                if column in headerList:
+                    if index not in headerIndex:
+                        headerIndex.append(index)
+                        tempObject = {column:[]}
+                    csvRead.update(tempObject)
+
+        # print (csvRead)
+        print(f"{i}"+" entries imported.")
+
+    j = 1
+    for entry in csvRead:
+        for value in entry:
+            tempObject = {
+                j: {
+                    entry: value
+                }
+        }
+        j += 1
+        MKSAKTobject.update(tempObject)
+    print(MKSAKTobject)
+    # filter_models_byProdGroup(root_path, prodGroups)
+
+
+# Build logic to find models to update - loop through object line by line
+#   - Check valid date range
+#       - Check against excluded product groups, brand & deliverers
+#   - Produce preview output list
 
 def contains_target_values(data, target_values):
     """
@@ -168,9 +186,9 @@ def contains_target_keys(data, target_keys):
 
 # Example usage:
 if __name__ == "__main__":
-    # Set your search root path and target values here
-    search_path = (r"F:\WebTools\walm.github.io\AktionspreisFixer\XML-Test")
+
+    root_path = (r"F:\WebTools\AktionspreisFixer\XML-Test")
     filter_path = (r"F:\WebTools\AktionspreisFixer\filter.json")
 
-    read_csv_file()
+    read_csv_file(root_path, prodGroups)
     # find_json_with_values(search_path, filter_path)
