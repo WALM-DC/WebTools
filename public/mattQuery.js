ecf0ef0558f06743aaaee21bb7cbf4b33dac46f5
Matthias Wallner <walm@xxxlutz.at>
2025-12-10 07:22:48 +0100

    updates

--- public/mattQuery.js
+++ public/mattQuery.js
@@ -2,5 +2,10 @@ function addZero(n) {return (n < 10)? '00' + n : (n < 100)? '0' + n : '' + n;}
 function injectTabs(){
     $.get('https://walm-dc.github.io/WebTools/public/tabs.html', function(data){
         $('#topDiv').html(data);
+        $('[link="'+window.location.href+'"]').addClass('active');
     });
+}
+function relinkToPage(this){
+    var link = $(this).attr('link');
+    window.location.href = link;
 }
